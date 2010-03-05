f02e09a2a279fdb9d1eda08539ce66ead26b4311
Ali Rantakari <ali.rantakari@hasseg.org>
2010-03-05 05:22:51 +0200

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,124 @@
+# trash makefile
+# 
+
+SHELL=/bin/bash
+
+CURRDATE=$(shell date +"%Y-%m-%d")
+SCP_TARGET=$(shell cat ./deploymentScpTarget)
+APP_VERSION=$(shell grep "^const int VERSION_" trash.m | awk '{print $$NF}' | tr -d ';' | tr '\n' '.' | sed -e 's/.$$//')
+VERSION_ON_SERVER=$(shell curl -Ss http://hasseg.org/trash/?versioncheck=y)
+TEMP_DEPLOYMENT_DIR=deployment/$(APP_VERSION)
+TEMP_DEPLOYMENT_ZIPFILE=$(TEMP_DEPLOYMENT_DIR)/trash-v$(APP_VERSION).zip
+TEMP_DEPLOYMENT_USAGEFILE="usage.txt"
+VERSIONCHANGELOGFILELOC="$(TEMP_DEPLOYMENT_DIR)/changelog.html"
+GENERALCHANGELOGFILELOC="changelog.html"
+SCP_TARGET=$(shell cat ./deploymentScpTarget)
+DEPLOYMENT_INCLUDES_DIR="./deployment-files"
+COMPILER="/Developer/usr/bin/clang"
+#COMPILER="gcc"
+
+
+
+all: trash
+
+docs: trash.1 usage.txt
+
+
+usage.txt: trash
+	@echo
+	@echo ---- generating usage.txt:
+	@echo ======================================
+	./trash > usage.txt
+
+
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+# compile the binary itself
+#-------------------------------------------------------------------------
+trash: trash.m
+	@echo
+	@echo ---- Compiling:
+	@echo ======================================
+	$(COMPILER) -O2 -Wall -force_cpusubtype_ALL -mmacosx-version-min=10.5 -arch i386 -arch ppc -framework AppKit -o $@ trash.m
+
+
+
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+# generate main man page from POD syntax file
+#-------------------------------------------------------------------------
+trash.1: trash.pod
+	@echo
+	@echo ---- Generating manpage from POD file:
+	@echo ======================================
+	pod2man --section=1 --release=1.0 --center="trash" --date="$(CURRDATE)" trash.pod > trash.1
+
+
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+# make release package (prepare for deployment)
+#-------------------------------------------------------------------------
+package: trash docs
+	@echo
+	@echo ---- Preparing for deployment:
+	@echo ======================================
+	
+# create zip archive
+	mkdir -p $(TEMP_DEPLOYMENT_DIR)
+	echo "-D -j $(TEMP_DEPLOYMENT_ZIPFILE) trash trash.1" | xargs zip
+	cd "$(DEPLOYMENT_INCLUDES_DIR)"; echo "-g -R ../$(TEMP_DEPLOYMENT_ZIPFILE) *" | xargs zip
+	
+# if changelog doesn't already exist in the deployment dir
+# for this version, get 'general' changelog file from root if
+# one exists, and if not, create an empty changelog file
+	@( if [ ! -e $(VERSIONCHANGELOGFILELOC) ];then\
+		if [ -e $(GENERALCHANGELOGFILELOC) ];then\
+			cp $(GENERALCHANGELOGFILELOC) $(VERSIONCHANGELOGFILELOC);\
+			echo "Copied existing changelog.html from project root into deployment dir - opening it for editing";\
+		else\
+			echo -e "<ul>\n	<li></li>\n</ul>\n" > $(VERSIONCHANGELOGFILELOC);\
+			echo "Created new empty changelog.html into deployment dir - opening it for editing";\
+		fi; \
+	else\
+		echo "changelog.html exists for $(APP_VERSION) - opening it for editing";\
+	fi )
+	@open -a Smultron $(VERSIONCHANGELOGFILELOC)
+
+
+
+
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+# deploy to server
+#-------------------------------------------------------------------------
+deploy: package
+	@echo
+	@echo ---- Deploying to server:
+	@echo ======================================
+	
+	@echo "Checking latest version number vs. current version number..."
+	@( if [ "$(VERSION_ON_SERVER)" != "$(APP_VERSION)" ];then\
+		echo "Latest version on server is $(VERSION_ON_SERVER). Uploading $(APP_VERSION).";\
+	else\
+		echo "NOTE: Current version exists on server: ($(APP_VERSION)).";\
+	fi;\
+	echo "Press enter to continue uploading to server or Ctrl-C to cancel.";\
+	read INPUTSTR;\
+	scp -r $(TEMP_DEPLOYMENT_DIR) $(TEMP_DEPLOYMENT_USAGEFILE) $(SCP_TARGET); )
+
+
+
+
+
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+clean:
+	@echo
+	@echo ---- Cleaning up:
+	@echo ======================================
+	-rm -Rf trash
+	-rm -Rf usage.txt
+	-rm -Rf trash.1
+
+
+
---
+++ trash.m
@@ -0,0 +1,217 @@
+//  trash.m
+//
+//  Created by Ali Rantakari
+//  http://hasseg.org
+//
+
+/*
+The MIT License
+
+Copyright (c) 2008-2010 Ali Rantakari
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+#include <AppKit/AppKit.h>
+#import <libgen.h>
+
+
+const int VERSION_MAJOR = 0;
+const int VERSION_MINOR = 9;
+const int VERSION_BUILD = 0;
+
+char *myBasename;
+
+
+
+// helper methods for printing to stdout and stderr
+
+void Print(NSString *aStr)
+{
+	if (aStr == nil)
+		return;
+	[aStr writeToFile:@"/dev/stdout" atomically:NO encoding:NSUTF8StringEncoding error:NULL];
+}
+
+void Printf(NSString *aStr, ...)
+{
+	va_list argList;
+	va_start(argList, aStr);
+	NSString *str = [
+		[[NSString alloc]
+			initWithFormat:aStr
+			locale:[[NSUserDefaults standardUserDefaults] dictionaryRepresentation]
+			arguments:argList
+			] autorelease
+		];
+	va_end(argList);
+	
+	[str writeToFile:@"/dev/stdout" atomically:NO encoding:NSUTF8StringEncoding error:NULL];
+}
+
+void PrintfErr(NSString *aStr, ...)
+{
+	va_list argList;
+	va_start(argList, aStr);
+	NSString *str = [
+		[[NSString alloc]
+			initWithFormat:aStr
+			locale:[[NSUserDefaults standardUserDefaults] dictionaryRepresentation]
+			arguments:argList
+			] autorelease
+		];
+	va_end(argList);
+	
+	[str writeToFile:@"/dev/stderr" atomically:NO encoding:NSUTF8StringEncoding error:NULL];
+}
+
+
+
+
+NSString* versionNumberStr()
+{
+	return [NSString stringWithFormat:@"%d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD];
+}
+
+void printUsage()
+{
+	Printf(@"usage: %s <file> ...\n", myBasename);
+	Printf(@"\n");
+	Printf(@"  Move files/folders to the trash.\n");
+	Printf(@"\n");
+	Printf(@"Version %@\n", versionNumberStr());
+	Printf(@"Copyright (c) 2010 Ali Rantakari, http://hasseg.org/\n");
+	Printf(@"\n");
+}
+
+// returns YES if success, NO if failure
+BOOL moveFileToTrash(NSString *filePath)
+{
+	if (filePath == nil)
+		return NO;
+	
+	NSString *fileDir = [filePath stringByDeletingLastPathComponent];
+	NSString *fileName = [filePath lastPathComponent];
+	
+	// in 10.5, this uses FSMoveObjectToTrashSync()
+	// 
+	return [[NSWorkspace sharedWorkspace]
+		performFileOperation:NSWorkspaceRecycleOperation
+		source:fileDir
+		destination:@""
+		files:[NSArray arrayWithObject:fileName]
+		tag:nil
+		];
+}
+
+
+
+int exitValue = 0;
+#define EXIT(x)		exitValue = x; goto cleanUpAndExit;
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *autoReleasePool = [[NSAutoreleasePool alloc] init];
+	
+	
+	myBasename = basename(argv[0]);
+	
+	if (argc == 1)
+	{
+		printUsage();
+		EXIT(0);
+	}
+	
+	NSMutableArray *URLs = [NSMutableArray arrayWithCapacity:(NSUInteger)argc];
+	NSMutableArray *paths = [NSMutableArray arrayWithCapacity:(NSUInteger)argc];
+	
+	int i;
+	for (i = 1; i < argc; i++)
+	{
+		// note: don't standardize the paths! we don't want to expand symlinks.
+		NSString *path = [[NSString stringWithUTF8String:argv[i]] stringByExpandingTildeInPath];
+		if (path == nil)
+		{
+			PrintfErr(@"Error: invalid path: %s\n", argv[i]);
+			continue;
+		}
+		NSURL *fileURL = [NSURL fileURLWithPath:path];
+		[URLs addObject:fileURL];
+		[paths addObject:path];
+	}
+	
+	//Printf(@" URLs: %@\n", URLs);
+	//Printf(@" paths: %@\n", paths);
+	
+	if ([URLs count] == 0)
+	{
+		printUsage();
+		EXIT(1);
+	}
+	else
+	{
+		NSFileManager *fm = [NSFileManager defaultManager];
+		
+		for (NSString *path in paths)
+		{
+			if (!moveFileToTrash(path))
+			{
+				if (![fm fileExistsAtPath:path])
+					PrintfErr(@"Error: path does not exist: %@\n", path);
+				else
+					PrintfErr(@"Error: can not delete %@\n", path);
+				
+				if (exitValue == 0)
+					exitValue = 1;
+			}
+		}
+		
+		// note: (from the docs)
+		// "This methods may show a progress indicator, or other user interface
+		// elements, at AppKit's discretion."
+		// 
+		// 10.6 only but doesn't seem to work (?)
+		// 
+		/*
+		[[NSWorkspace sharedWorkspace]
+			recycleURLs:URLs
+			completionHandler:^(NSDictionary *newURLs, NSError *error)
+			{
+				if (error != nil)
+				{
+					PrintfErr(@"Error: %@\n", [error localizedDescription]);
+				}
+			}];
+		*/
+	}
+	
+	
+cleanUpAndExit:
+	[autoReleasePool release];
+	return exitValue;
+}
+
+
+
+
+
+
+
+
