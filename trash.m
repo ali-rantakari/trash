1017bbcb509239d2837ba2db2b862a5fb39f8f94
Ali Rantakari <ali.rantakari@hasseg.org>
2010-03-06 23:55:25 +0200

    Fix copyright year.

--- trash.m
+++ trash.m
@@ -7,7 +7,7 @@
 /*
 The MIT License
 
-Copyright (c) 2008-2010 Ali Rantakari
+Copyright (c) 2010 Ali Rantakari
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
